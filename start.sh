e7cdb9f33a1029a7c4ea89750e5f1a500d993ef4
vibin200 <121194171+vibin200@users.noreply.github.com>
2022-12-22 23:24:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/CrazyDeveloperTG/Doctor-Strange.git /Doctor-Strange
+  git clone https://github.com/vibin200/Doctor-Strange /Doctor-Strange
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Doctor-Strange
